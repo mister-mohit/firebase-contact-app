29e80c822b4179a56206fa1a4fabf3b1f49c5a99
Mohit <mohitx618@gmail.com>
2023-11-12 16:06:29 +0530

    Search and Navigation bar Css complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {AiOutlineSearch} from "react-icons/ai";
+import {MdAddCircle} from "react-icons/md";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+  <div className="max-w-[360px] mx-auto p-2">
+
+    {/*Div for the navigation bar */ }
+    <div className="flex bg-white rounded-md h-[60px] justify-center items-center gap-2 ">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src="logos_firebase.png" alt="logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1 className="text-2xl font-bold ">Firebase Contact App</h1>
+    </div>
+
+    {/*div for the search bar */}
+    <div className="mt-4 flex ">
+      <div className="relative flex flex-grow items-center text-white">
+        <AiOutlineSearch className="text-2xl absolute top-[28%] left-1"/>
+        <input className="h-[40px] rounded-md bg-transparent border-white border-2 pl-7 placeholder-white flex-grow " placeholder="Search contact" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <MdAddCircle className="text-5xl" />
+    </div>
+
+    {/*div for contact cards*/}
+    
+
+  </div>
+    );
 }
 
-export default App
+export default App;
